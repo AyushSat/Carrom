c431c6200ff16b812f1ff146a68e252bfe81c38e
CalixTang <ctang936@student.fuhsd.org>
2019-05-06 12:58:51 -0700

    - Runnable Carrom (Main) Class      - PApplet in JFrame      - Default 1000x1000, non-resizable  - Documented Carrom (Main) Class

--- Carrom/src/Carrom.java
+++ Carrom/src/Carrom.java
@@ -1,4 +1,33 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
 
+/**Carrom is the main class of the project. When run, it will launch the actual game
+ * in a window. 
+ * 
+ * @author Calix Tang, Akshat Jain, and Ayush Satyavarpu
+ *
+ */
 public class Carrom {
-	
+	public static void main(String[] args) {
+		
+		//initialize and run sketch
+		GameBoard board = new GameBoard();
+		PApplet.runSketch(new String[]{"Carrom"}, board);
+		
+		PSurfaceAWT surf = (PSurfaceAWT) board.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+		
+		//window is 1000x1000 permanently
+		window.setSize(1000,1000);
+		window.setMinimumSize(new Dimension(1000,1000));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);//a stretch is to change this
+		
+		//make window visible
+		window.setVisible(true);
+		canvas.requestFocus();
+	}
 }
