8ece245a9eec950c1899b09d9e79a73f41581cc7
ajain027 <ajain027@PR10.hhs.win.fuhsd.org>
2019-05-24 14:34:50 -0700

    credits img

--- Carrom/src/Credits.java
+++ Carrom/src/Credits.java
@@ -3,6 +3,11 @@ import java.io.File;
 import processing.core.PApplet;
 import processing.core.PImage;
 
+/**
+ * 
+ * @author ajain027
+ *
+ */
 public class Credits extends PApplet{
 
 	private float width;
@@ -26,16 +31,7 @@ public class Credits extends PApplet{
 		//img = loadImage("data" + File.separator + "");
 	}
 	public void draw() {
-		//image(img, 0, 0);
-		background(255);
-		fill(0);
-		textSize(50);
-		textAlign(CENTER,CENTER);
-		text("Code and backend by Calix",width/2,height/3);
-		text("Images by Akshat",width/2,height/2);
-		text("Networking by Ayush",width/2,height*2/3);
-		textSize(10);
-		text("hppeng",width*15/16,height*15/16);
+		image(img, 0, 0);
 	}
 	public void keyPressed() {
 
