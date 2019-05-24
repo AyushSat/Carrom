7c73baf8a26144ba65ab47234ad9f8e82973db98
CalixTang <calix.tang@gmail.com>
2019-05-23 21:02:00 -0700

    Numbers on player sides added

--- Carrom/src/GameBoard.java
+++ Carrom/src/GameBoard.java
@@ -469,21 +469,7 @@ public class GameBoard extends PApplet {
 		// text(keyCode,100,100);
 	}
 
-	/*
-	 * public static void main(String[] args) { Tester board = new Tester(9, 9);
-	 * PApplet.runSketch(new String[]{"Carrom"}, board);
-	 * 
-	 * PSurfaceAWT surf = (PSurfaceAWT) board.getSurface(); PSurfaceAWT.SmoothCanvas
-	 * canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative(); JFrame window =
-	 * (JFrame)canvas.getFrame();
-	 * 
-	 * //window is 1000x1000 permanently window.setSize(1000,1000);
-	 * window.setMinimumSize(new Dimension(1000,1000));
-	 * window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 * window.setResizable(false);//a stretch is to change this
-	 * 
-	 * //make window visible window.setVisible(true); canvas.requestFocus(); }
-	 */
+	
 	public void exit() {
 		this.dispose();
 	}
